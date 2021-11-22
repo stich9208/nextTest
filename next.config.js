a51d3da2dd81fe1063d28a877625282d713ebc40
JaewookJung <stich9208@gmail.com>
2021-11-22 12:14:53 +0900

    Add: gzip 번들 압축 적용
    Signed-off-by: JaewookJung <stich9208@gmail.com>

--- next.config.js
+++ next.config.js
@@ -3,7 +3,13 @@
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
+const CompressionPlugin = require("compression-webpack-plugin");
 
 module.exports = withBundleAnalyzer({
   reactStrictMode: true,
+  compress: true,
+  webpack: function (config) {
+    config.plugins.push(new CompressionPlugin());
+    return config;
+  },
 });
